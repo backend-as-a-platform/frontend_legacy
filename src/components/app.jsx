1a962a04acf974dba7c360e1e817198143c545a3
arjunshibu <arjunshibu1999@gmail.com>
2022-03-26 14:21:46 +0530

    initialized boilerplate project

---
+++ src/components/app.jsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+import '../css/app.css';
+
+export default class App extends Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { render } from 'react-dom';
+import App from './components/app';
+import './css/index.css';
+
+render(<App />, document.querySelector('#root'));
