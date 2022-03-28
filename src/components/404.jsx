f73868cff61c8106af7458cbcc5dc9b9d32af6d3
arjunshibu <arjunshibu1999@gmail.com>
2022-03-28 13:06:11 +0530

    added 404

---
+++ src/components/404.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import '../css/404.css';
+
+const NotFound = () => (
+  <div id="notfound">
+    <div className="notfound">
+      <div className="notfound-404">
+        <h3>Oops! Page not found</h3>
+        <h1>
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </h1>
+      </div>
+      <h2>
+        come on, let's get you <Link to="/">home</Link>.
+      </h2>
+    </div>
+  </div>
+);
+
+export default NotFound;
