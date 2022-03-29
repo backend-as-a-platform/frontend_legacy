86d7d775a9d9ff4f1b31bafdcf2ee484893999e7
arjunshibu <arjunshibu1999@gmail.com>
2022-03-29 23:12:15 +0530

    changed 404 style

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,11 +1,9 @@
-import { Fragment } from 'react';
 import Navbar from './nav-bar';
 
 const About = () => (
-  <Fragment>
-    <Navbar />
+  <>
     <h1 className="mt-5 mb-4">About</h1>
-  </Fragment>
+  </>
 );
 
 export default About;
